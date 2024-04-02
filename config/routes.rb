6199ef77b7170cab62b07c6ea260c31ac03e19f6
p3rc1us <kervymaniego@gmail.com>
2024-04-02 15:54:15 +0800

    chore: added iex gem, login page running

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
-  get 'pages/home'
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions'
+  }
+
+  devise_for :users,  controllers: {
+    sessions: 'users/sessions'
+  }
+
+  root 'pages#home'
   get 'pages/users'
   get 'pages/admin'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
