2f10445def0abae65fed2cba9787ad0ff245000b
p3rc1us <kervymaniego@gmail.com>
2024-04-08 11:39:07 +0800

    feat: Admin can toggle approval of User in Edit User page, User now receives email when approved by admin

---
+++ app/mailers/approved_mailer.rb
@@ -0,0 +1,13 @@
+class ApprovedMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.approved_mailer.is_approved.subject
+  #
+  def is_approved
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
